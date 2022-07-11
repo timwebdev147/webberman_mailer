e1cc0718292c8bbcb6194a3b54da9d33a82a673d
timwebdev147 <Timwebdev147@gmail.com>
2022-07-11 22:30:35 +0100

    my first commit

--- index.php
+++ index.php
@@ -1,5 +1,45 @@
-<?php
+<?php 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-echo 'hello there heroku, Akinsanmidev here!'
+require '/PHPMailer/src/Exception.php';
+require '/PHPMailer/src/PHPMailer.php';
+require '/PHPMailer/src/SMTP.php';
 
-?>
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.office365.com';                  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'webbermanquery@outlook.com';             // SMTP username
+    $mail->Password = 'Nimifavour1';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable SSL encryption, TLS also accepted with port 465
+    $mail->Port = 25;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('webbermanquery@outlook.com', 'webbermanquery');          //This is the email your form sends From
+    $mail->addAddress('akinsanmidev@gmail.com', 'Akinsanmi Timothy'); // Add a recipient address
+    //$mail->addAddress('contact@example.com');               // Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Subject line goes here';
+    $mail->Body    = 'Body text goes here';
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
+?> 
