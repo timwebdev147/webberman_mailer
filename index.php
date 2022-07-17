3b46233bcc411c6228024fe1e0a5825cbc58491b
timwebdev147 <Timwebdev147@gmail.com>
2022-07-17 01:33:21 +0100

    made the Ui better

--- index.php
+++ index.php
@@ -1,45 +1,39 @@
-<?php 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+<?php
 
-require '/PHPMailer/src/Exception.php';
-require '/PHPMailer/src/PHPMailer.php';
-require '/PHPMailer/src/SMTP.php';
 
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
-    //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.office365.com';                  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'webbermanquery@outlook.com';             // SMTP username
-    $mail->Password = 'Nimifavour1';                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable SSL encryption, TLS also accepted with port 465
-    $mail->Port = 25;                                    // TCP port to connect to
 
-    //Recipients
-    $mail->setFrom('webbermanquery@outlook.com', 'webbermanquery');          //This is the email your form sends From
-    $mail->addAddress('akinsanmidev@gmail.com', 'Akinsanmi Timothy'); // Add a recipient address
-    //$mail->addAddress('contact@example.com');               // Name is optional
-    //$mail->addReplyTo('info@example.com', 'Information');
-    //$mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
+//     $fullname = htmlspecialchars($_POST['fullname']);
+// 	// $last_name = htmlspecialchars($_POST['last_name']);
+// 	$email = $_POST['email'];
+//     $phoneNumber = $_POST['number'];
+// 	// $url = htmlspecialchars($_POST['url']);
+// 	$message = json_decode( htmlspecialchars($_POST['note']) );
 
-    //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-    //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Subject line goes here';
-    $mail->Body    = 'Body text goes here';
-    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
+include ('db.php');
+mysqli_select_db($conn,"sql11506563");
+
+// mysqli_select_db($link,"personal_info");
+if(isset($_REQUEST['submit']))
+{
+    $errorMessage = "";
+    $fname=$_POST['fullname'];
+    $email=$_POST['email'];
+    $numbers = $_POST['number'];
+    $comments = $_POST['comment'];
+    // other variables and validation here
+    if ($errorMessage != "" ) {
+        echo "<p class='message'>" .$errorMessage. "</p>" ;
+    }
+    else{
+        //Inserting record in table using INSERT query
+        $insertTB="INSERT INTO `sql11506563`.`MyClients`
+        (`fullname`, `Email`, `numbers`, `comments`) VALUES ('$fname', '$email', '$numbers', '$comments')";
+        //mysqli_query($insertTB);
+        mysqli_query($conn,$insertTB) or die(mysqli_error($conn));
+    }
+    header("Location: http://localhost:8080/", true, 301);
+exit();
 }
-?> 
+?>
