3436ab8a668fcc4621bb5c04495e80e6deb55c0f
Akinsanmi Timothy <timwebdev147@gmail.com>
2022-10-09 13:46:02 +0100

    changes

--- api/index.php
+++ api/index.php
@@ -1,15 +1,15 @@
 <?php
 phpinfo();
 
-if(isset($_REQUEST['submit']))
-{
+// if(isset($_REQUEST['submit']))
+// {
 
-    $fname=$_POST['fullname'];
-    $email=$_POST['email'];
-    $numbers = $_POST['number'];
-    $comments = $_POST['comment'];
-    // other variables and validation here
+//     $fname=$_POST['fullname'];
+//     $email=$_POST['email'];
+//     $numbers = $_POST['number'];
+//     $comments = $_POST['comment'];
+//     // other variables and validation here
    
-    header("Location: https://webberman.vercel.app/", true, 301);
-exit();
-}
+//     header("Location: https://webberman.vercel.app/", true, 301);
+// exit();
+// }
