e0a2fe862ec13ad4d0b8d9a862fc1a0f21bd4cee
Akinsanmi Timothy <timwebdev147@gmail.com>
2022-10-09 13:35:34 +0100

    pushing to git

---
+++ api/index.php
@@ -0,0 +1,15 @@
+<?php
+phpinfo();
+
+if(isset($_REQUEST['submit']))
+{
+
+    $fname=$_POST['fullname'];
+    $email=$_POST['email'];
+    $numbers = $_POST['number'];
+    $comments = $_POST['comment'];
+    // other variables and validation here
+   
+    header("Location: https://webberman.vercel.app/", true, 301);
+exit();
+}
