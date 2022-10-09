27dff6607c21d193e499b3b504802a97381ba926
Akinsanmi Timothy <timwebdev147@gmail.com>
2022-10-10 00:52:05 +0100

    changes

--- api/index.php
+++ api/index.php
@@ -7,7 +7,7 @@ use PHPMailer\PHPMailer\Exception;
 
 // If necessary, modify the path in the require statement below to refer to the
 // location of your Composer autoload.php file.
-require __DIR__ . '/../../../vendor/autoload.php';
+require __DIR__ . '../vendor/autoload.php';
 
 // Replace sender@example.com with your "From" address.
 // This address must be verified with Amazon SES.
